dcfc9a21a59512b9af0f70efed6e7cddcb06b40b
TamaraMoraisF <tamaramoraisfrigo@gmail.com>
2024-04-13 15:38:28 -0400

    Adiciono script para baixar XML

--- feed.py
+++ feed.py
@@ -1,3 +1,20 @@
+import requests
+
+# URL do feed XML
+url = 'https://storage.cloud.google.com/psel-feedmanager/feed_psel.xml'
+
+# Faz o download do arquivo XML
+response = requests.get(url)
+
+# Verifica se a requisição foi bem-sucedida
+if response.status_code == 200:
+    # Salva o conteúdo da resposta em um arquivo local
+    with open('feed_fake.xml', 'wb') as f:
+        f.write(response.content)
+        print('Arquivo XML baixado com sucesso!')
+else:
+    print('Falha ao baixar o arquivo XML.')
+
 # Excluir produtos fora de estoque
 
 # Adicionar cor aos produtos especificados
