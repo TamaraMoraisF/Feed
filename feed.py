23b9c16f418aecf4697fc350f3a4988fa509500c
TamaraMoraisF <tamaramoraisfrigo@gmail.com>
2024-04-13 22:28:24 -0400

    Adiciona script de remover produtos fora do estoque

--- feed.py
+++ feed.py
@@ -1,4 +1,5 @@
 import requests
+import xml.etree.ElementTree as ET
 
 # URL do feed XML
 url = 'https://storage.cloud.google.com/psel-feedmanager/feed_psel.xml'
@@ -9,9 +10,28 @@ response = requests.get(url)
 # Verifica se a requisição foi bem-sucedida
 if response.status_code == 200:
     # Salva o conteúdo da resposta em um arquivo local
-    with open('feed_fake.xml', 'wb') as f:
+    with open('feed_.xml', 'wb') as f:
         f.write(response.content)
         print('Arquivo XML baixado com sucesso!')
+
+    try:
+        # Abre o arquivo XML
+        tree = ET.parse('feed_psel.xml')
+        root = tree.getroot()
+
+        # Excluir produtos fora de estoque
+        for product in root.findall('product'):
+            stock = product.find('stock').text
+            if int(stock) <= 0:
+                root.remove(product)
+
+        # Salva as alterações em um novo arquivo XML
+        tree.write('feed_updated.xml')
+        print('Produtos fora de estoque removidos com sucesso!')
+
+    except ET.ParseError as e:
+        print(f'Erro ao abrir o arquivo XML: {e}')
+
 else:
     print('Falha ao baixar o arquivo XML.')
 
