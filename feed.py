3fc5065a261616b3fd6e4880ca48ffa36c1d5b0f
TamaraMoraisF <tamaramoraisfrigo@gmail.com>
2024-04-14 12:42:54 -0400

    Corrige  links das imagens

--- feed.py
+++ feed.py
@@ -1,4 +1,6 @@
 import requests
+import random
+import string
 import xml.etree.ElementTree as ET
 
 # URL do feed XML
@@ -37,8 +39,21 @@ if response.status_code == 200:
                 last_word = name.split()[-1]  # Obtém a última palavra do título
                 product.find('color').text = last_word
 
+        # IDs dos produtos para corrigir os links de imagem
+        product_ids_images = ['246804', '217865']
+
+        # Corrigir os links das imagens para os IDs especificados
+        for product in root.findall('product'):
+            product_id = product.find('id').text
+            if product_id in product_ids_images:
+                # Gerar um link de imagem aleatório para cada ID
+                random_suffix = ''.join(random.choices(string.ascii_lowercase + string.digits, k=2))
+                new_link = f'https://example.com/new_image_{random_suffix}.jpg'
+                product.find('image_link').text = new_link
+                print(f'Link de imagem corrigido para o produto ID {product_id}: {new_link}')
+
         tree.write('feed_updated.xml')
-        print('Produtos sem estoque removidos e tags color atualizadas com sucesso!')
+        print('Certo')
 
     except ET.ParseError as e:
         print(f'Erro ao abrir o arquivo XML: {e}')
@@ -46,12 +61,4 @@ if response.status_code == 200:
 else:
     print('Falha ao baixar o arquivo XML.')
 
-
-
-
-# Excluir produtos fora de estoque
-
-# Adicionar cor aos produtos especificados
-
-# Corrigir links das imagens dos produtos com IDs específicos
 
