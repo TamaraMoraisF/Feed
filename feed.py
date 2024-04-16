345cf7d177f56438ce7b3142fa121ffc7a467908
TamaraMoraisF <tamaramoraisfrigo@gmail.com>
2024-04-16 11:41:22 -0400

    Passo a utilizar o XML baixado

--- feed.py
+++ feed.py
@@ -5,66 +5,71 @@ import xml.etree.ElementTree as ET
 
 def download_xml(url, filename):
     response = requests.get(url)
+
     if response.status_code == 200:
         with open(filename, 'wb') as f:
             f.write(response.content)
-        print('Arquivo XML baixado com sucesso!')
         return True
     else:
-        print('Falha ao baixar o arquivo XML.')
+        print("Falha ao obter o XML:", response.status_code)
         return False
 
-def remover_produtos_sem_estoque(root):
-    for produto in root.findall('product'):
-        estoque = int(produto.find('stock').text)
-        if estoque <= 0:
-            root.remove(produto)
+def remover_produtos_sem_estoque(root, item):
+    estoque = item.find('availability').text
+    if estoque != '"Em estoque"':
+        root.remove(item)
 
-def adicionar_cor_a_produtos(id_produto, ids_produtos_cor, produto):
-    if id_produto in ids_produtos_cor:
-        nome = produto.find('tittle').text
+def adicionar_cor_a_produtos(id_item, ids_produtos_cor, item):
+    if id_item in ids_produtos_cor:
+        nome = item.find('title').text
         ultima_palavra = nome.split()[-1]
-        produto.find('color').text = ultima_palavra
+        item.find('color').text = ultima_palavra
 
-def corrigir_links_de_imagem(id_produto, ids_produtos_imagens, produto):
-    if id_produto in ids_produtos_imagens:
+contador_sufixo = {}
+
+def corrigir_links_de_imagem(id_item, ids_produtos_imagens, item):
+    global contador_sufixo
+
+    if id_item in ids_produtos_imagens:
+        if id_item not in contador_sufixo:
+            contador_sufixo[id_item] = 0
+        
         sufixo_aleatorio = ''.join(random.choices(string.ascii_lowercase + string.digits, k=6))
-        novo_link = f'https://example.com/new_image_{sufixo_aleatorio}.jpg'
-        produto.find('image_link').text = novo_link
-        print(f'Link de imagem corrigido para o produto ID {id_produto}: {novo_link}')
+        novo_link = f'https://example.com/new_image_{id_item}_{sufixo_aleatorio}.jpg'
+        
+        contador_sufixo[id_item] += 1
+        
+        item.find('image_link').text = novo_link
+        print(f'Link de imagem corrigido para o item ID {id_item}: {novo_link}')
+
 
 def processar_xml(root):
-    for produto in root.findall('product'):
-        remover_produtos_sem_estoque(root)
+    for item in root.findall('item'):
+        remover_produtos_sem_estoque(root, item)
 
-        id_produto = produto.find('id').text
+        id_item = item.find('id').text
 
         ids_produtos_cor = ['261557', '235840']
-        adicionar_cor_a_produtos(id_produto, ids_produtos_cor, produto)
+        adicionar_cor_a_produtos(id_item, ids_produtos_cor, item)
 
         ids_produtos_imagens = ['246804', '217865']
-        corrigir_links_de_imagem(id_produto, ids_produtos_imagens, produto)
+        corrigir_links_de_imagem(id_item, ids_produtos_imagens, item)
 
 def salvar_xml(root, nome_arquivo):
     with open(nome_arquivo, 'wb') as f:
         f.write(b'<?xml version="1.0" encoding="UTF-8"?>\n')
-
-        for child in root.findall('feed'):
-            root.remove(child)
-
         nova_arvore = ET.ElementTree(root)
         nova_arvore.write(f, encoding='utf-8', xml_declaration=False, default_namespace=None, short_empty_elements=True)
 
     print('Arquivo XML corrigido com sucesso.')
 
 # URL do feed XML e nome do arquivo local
-url = 'https://storage.cloud.google.com/psel-feedmanager/feed_psel.xml'
+url = 'https://ff401d59c2a65eca56e0556319a230613acacb6808cee9dddf9201b-apidata.googleusercontent.com/download/storage/v1/b/psel-feedmanager/o/feed_psel.xml?jk=AWNiL9LLVdJGh0Co9PTJzhKJpMxLJk47hY1n8gkXUlca6j-h26XCqa1-76se6VLHqr5cdbS69vrWxyppjvDkj7kinPYBPQwnVuUZxbrJChaqwPO4yQSA5lrj1GG4DeDV8AdmLM-q4b9wxByQDW7ZlzGwcxv80mAqynyvvO62os4qV8JeMXl0q00LNr0iI5zMnAQfJqT00m_j6OcZlBN69d86B4hoti0WpN90t-ck-vtSWri5ge4EeJ_Wh6ITDaxvUKYgWAaCbCQ4Y44pChQZtogWTrA0WxHFVUp5cmr6boyPeS5asl4Cr9z-r8vw-GufEWruffuiw3j4UQamRU8RO_ive7yv6Xet-0Uq-mk-d5_W2KPkihaMdEczgGo5SY0p6AsygVD4XzcdgnNAiQQ8O9lcISNUkUC7CgmEyQENtXfsMSMrfoM_ntOMsBZ2ZBw-tTPZoF2mxe9xY2_gX7Xw0XcTWiiL-LjSd8-5Yt_qgsD_mRIN-cfUXbYs-M_re-okLse-dWY_b8Pcws_DhGyC6f0b_F-XwDGMC995kvaB_uAy6pGz_MDIa6HFhY4UwwXr8fYbqob4kdp6daHH6x5eZ2xa74Yk8C1nyJz4OEduTRl31Itu0ixu7DFpWcquf2wEemf-BXoCOANKrHps9tMDJXDQfsy8YWT4IDEyz5wAfLgUjAZ8owz1CWNfyVNvn9oJ5SFR5C1BuyDc7wbfXfywcyxzb7hgF6v7k7DYElOKnPj9yIQS3ychOojZGS0kDjoJ5A13MtQqa41culebF3adBkAq2JUfmhUb1mkHkH-E2yHz6MfF75nCBQ3f5dWS4fYruaz2dLnj_MltZz4qjQ4ZcYxx-L56TJsU8C3cc2QIkD4VyCPz4sU_O1mjATe4NUy1wcOl6DVROn1P7IBN7meF2jrEB1AhU4HpgyVSixqJOtZXUXHic28uHf4KWRF5Xtd-UrmBAL-1bGU24bzz-5kEO0whXQYPgJfi4LGBhD7FSPAs8EKSCuddSmNQFMnRLlYZK358vifG2TH-73bFThDFGP5RCjbOOiV-wjB8_sNbsSxLzBkKKr3sxqlvZZrpA3CtBqJc0ExHDwDHgxL44k399uGimQAkY94PDw&isca=1'
 xml_feed = 'feed_psel.xml'
-xml_exemplo = 'feed_psel_exemplo.xml'
 
 # Download do arquivo XML
 if download_xml(url, xml_feed):
-    arvore = ET.parse(xml_exemplo)
+    arvore = ET.parse(xml_feed)
     raiz = arvore.getroot()
 
     processar_xml(raiz)
--- teste.py
+++ teste.py
@@ -1,13 +1,13 @@
 import unittest
-from unittest.mock import patch, MagicMock
 import xml.etree.ElementTree as ET
+from unittest.mock import patch
 import feed
 
 class TestFeedFunctions(unittest.TestCase):
     @patch('feed.requests.get')
     def test_download_xml_success(self, mock_get):
         mock_get.return_value.status_code = 200
-        mock_get.return_value.content = b'<products><product><id>1</id><stock>5</stock></product></products>'
+        mock_get.return_value.content = b'<produtos><item><id>1</id><availability>"Em estoque"</availability></item></produtos>'
         result = feed.download_xml('https://example.com/feed.xml', 'feed.xml')
         self.assertTrue(result)
 
@@ -18,41 +18,45 @@ class TestFeedFunctions(unittest.TestCase):
         self.assertFalse(result)
 
     def test_remover_produtos_sem_estoque(self):
-        xml_string = '<products><product><id>1</id><stock>0</stock></product><product><id>2</id><stock>5</stock></product></products>'
+        xml_string = '<produtos><item><id>1</id><availability>"Em estoque"</availability></item><item><id>2</id><availability>"Fora de estoque"</availability></item></produtos>'
         root = ET.fromstring(xml_string)
-        feed.remover_produtos_sem_estoque(root)
-        self.assertEqual(len(root.findall('product')), 1)
+        item_a_remover = root.find("item[availability='\"Fora de estoque\"']")
+        feed.remover_produtos_sem_estoque(root, item_a_remover)
+        items_em_estoque = root.findall("item[availability='\"Fora de estoque\"']")
+        self.assertEqual(len(items_em_estoque), 0)
+
     
     def test_adicionar_cor_a_produtos(self):
-        produto = ET.Element('product')
-        id_produto = ET.SubElement(produto, 'id')
+        item = ET.Element('item')
+        id_produto = ET.SubElement(item, 'id')
         id_produto.text = '261557'
-        nome = ET.SubElement(produto, 'tittle')
+        nome = ET.SubElement(item, 'title')
         nome.text = 'Camisa Azul'
-        cor = ET.SubElement(produto, 'color')
-        feed.adicionar_cor_a_produtos('261557', ['261557'], produto)
+        cor = ET.SubElement(item, 'color')
+        feed.adicionar_cor_a_produtos('261557', ['261557'], item)
         self.assertEqual(cor.text, 'Azul')
 
     def test_corrigir_links_de_imagem(self):
-        produto = ET.Element('product')
-        id_produto = ET.SubElement(produto, 'id')
+        item = ET.Element('item')
+        id_produto = ET.SubElement(item, 'id')
         id_produto.text = '246804'
-        link_imagem = ET.SubElement(produto, 'image_link')
-        feed.corrigir_links_de_imagem('246804', ['246804'], produto)
+        link_imagem = ET.SubElement(item, 'image_link')
+        feed.corrigir_links_de_imagem('246804', ['246804'], item)
         self.assertTrue(link_imagem.text.startswith('https://example.com/new_image_'))
 
     def test_processar_xml(self):
-        xml_string = '<products><product><id>1</id><stock>0</stock></product><product><id>2</id><stock>5</stock></product></products>'
+        xml_string = '<produtos><item><id>1</id><availability>"Em estoque"</availability></item><item><id>2</id><availability>"Fora de estoque"</availability></item></produtos>'
         root = ET.fromstring(xml_string)
         feed.processar_xml(root)
-        self.assertEqual(len(root.findall('product')), 1)
-    
+        self.assertEqual(root.find('item/id').text, '1')
+
+
     def test_salvar_xml(self):
         root = ET.Element('feed')
         feed.salvar_xml(root, 'teste.xml')
         with open('teste.xml', 'r') as f:
             content = f.read()
             self.assertIn('<feed />', content)
-            
+
 if __name__ == '__main__':
     unittest.main()
