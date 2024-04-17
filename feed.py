10c88ebdf671b3b9121681a731bbe65ac61fe802
TamaraMoraisF <tamaramoraisfrigo@gmail.com>
2024-04-17 19:23:22 -0400

    Corrige link de download de XML

--- feed.py
+++ feed.py
@@ -11,7 +11,7 @@ def download_xml(url, filename):
     else:
         print("Falha ao obter o XML:", response.status_code)
         return False
-
+    
 def remover_produtos_sem_estoque(root, item):
     estoque = item.find('availability').text
     if estoque != '"Em estoque"':
@@ -51,16 +51,17 @@ def salvar_xml(root, nome_arquivo):
 
     print('Arquivo XML corrigido com sucesso.')
 
-# URL do feed XML e nome do arquivo local
-url = 'https://ff06c38d0d5fb6cd1061f0be32d425c9ca90486fbad802ef2d697b4-apidata.googleusercontent.com/download/storage/v1/b/psel-feedmanager/o/feed_psel.xml?jk=AWNiL9J7fcU0wifZa6Na8sdOUNMbaecDwuq4vYazphRAX9o2dTynrGFvwBsP1lCGobUUSqPeJdeyKsKSP021mMocGSXhpDsY6uzaMEHqkdm6jxhMW49oqKXrZB9K6kRm3muu-o3EqEWW9sR3zKHqGoZvsZGU_YG_1Vs4p-6FlRPF8wkHQ6L457CUBJRxtAlo-CRnUIDkUijrQvQKFQC7byz8cWybB-qYWpKr8F8P3Vtb151trjv35eMGeR1Jr-kr0PtQ59IQ_Rbwpznh0TYT2yX6o_c6sw1MkGTwJLyF5AaAokuJiaAv2i5BPKW6kA8EBrhqlaTDis7Tav-e67uaCjxLKgNA1wMlUPf7cDXY-aM8DfnyRPrteW3NQv3lxWtkSHToe6ny3D4BxqjC8ZrvCcDi7dnteFJquHmx9EWVHNNFcOrElmUz6RXHwof9FwltGQyuiS2rl_vIhQX_Zqqrm8nxGX2HLt_UhkD8TpqXu4VY6CzK9aC-zL0aLI_fMBgbK8L0rxbEMN5XGjEf4ick4y2Ichp1fsejc2rejO0-m2_AUP40K19civSKMKENImcwzcGMmMMqe1VypO932Fsoc6mBvOjqBIOcaeD_5Y0cjRJyByvBIHUg9dWRLhgH3koHIiPdyn814x6hRXl300MrUbMWSShPS91tc85aF0x4VXrgwSRkCYB1Omv4KyckNCfGSCvIVbqQOe_xvBS9t5ogKg1bpYhZY_IyRSUNmtXIo8ewd_ZUxPM2nUtHqydA0hHv8UvnVax3597_D5Nqw__6x8wsZ2MSbNVrPfnPjiQPEhJ2R98YoIbdqLdJAryCkxgFvSu8qZOx54QA8lqWFkI5St6x3j3bRMUNUbkTL8YIM84C9VtSvmemvnP5lMRWp_9XCpS_V4qkok6uU70DVS7Z6KwSfh-WgOfCSU5bJqSpFsToi0zxWN8XpHtNCmCeBIp9XQnOLV0yUPR1lgBNkxTnrPALB1M2D7E-wn4E_roS5bFkZmwyjJJpahlypc48z_1mGc-W83LgAURYFE6Ddm1Esi5qQV5CyTZVCUXMoLotKDxzOSBG-LPU4r36dOcJKFLSbva6GFf9p1nQSAAkuXodf4uJwd90mjattA&isca=1'
-xml_feed = 'feed_psel.xml'
+# URL direta de download do XML no Google Cloud Storage
+url_xml = 'https://storage.googleapis.com/psel-feedmanager/feed_psel.xml'
 
 # Download do arquivo XML
-if download_xml(url, xml_feed):
-    arvore = ET.parse(xml_feed)
+if download_xml(url_xml, 'feed_psel.xml'):
+    arvore = ET.parse('feed_psel.xml')
     raiz = arvore.getroot()
 
     processar_xml(raiz)
 
     nome_arquivo_corrigido = 'feed_corrigido.xml'
     salvar_xml(raiz, nome_arquivo_corrigido)
+else:
+    print("Não foi possível baixar o XML.")
